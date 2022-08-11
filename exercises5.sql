a849418e872d00705bd72295f12e8cf0f0499e1d
igortega <75392391+igortega@users.noreply.github.com>
2022-08-11 17:15:00 +0200

    chapter 5 exercise 3

--- exercises5.sql
+++ exercises5.sql
@@ -18,3 +18,17 @@ FROM account a INNER JOIN customer c
     INNER JOIN product p 
     ON a.product_cd = p.product_cd
 WHERE c.cust_type_cd = 'I';
+
+
+-- Exercise 5-3
+-- Construct a query that finds all employees whose supervisor is assigned to a different
+-- department. Retrieve the employees’ ID, first name, and last name.
+
+SELECT e.emp_id, e.fname, e.lname, b.name emp_branch, sup_e.emp_id sup_id, sup_e.fname sup_fname, sup_e.lname sup_lname, sup_b.name sup_branch
+FROM employee e INNER JOIN branch b 
+	ON e.assigned_branch_id = b.branch_id
+    INNER JOIN employee sup_e 
+    ON sup_e.emp_id = e.superior_emp_id
+    AND sup_e.assigned_branch_id != e.assigned_branch_id
+    INNER JOIN branch sup_b 
+    ON sup_e.assigned_branch_id = sup_b.branch_id;
